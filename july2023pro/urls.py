004d0d3c4fb4a542f6b237f54a68f4790093fee1
h1y1d1 <varmavegasana@gmail.com>
2023-08-15 12:05:43 -0400

    views added

--- july2023pro/urls.py
+++ july2023pro/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from julyapp.views import demo
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("demo/", demo),
 ]
